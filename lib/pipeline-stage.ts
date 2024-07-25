582e7b5e09c8dbc4eaedf405f1eb338930b5404c
Vandana Bisht <vandana.bisht@accolitedigital.com>
2024-07-25 20:22:44 +0530

    changed webservice  name

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -6,6 +6,6 @@ export class WorkshopPipelineStage extends Stage {
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
-        new CdkWorkshopVandanaBishtStack(this, 'WebService');
+        new CdkWorkshopVandanaBishtStack(this, 'WebServiceVandanaBisht');
     }
 }
