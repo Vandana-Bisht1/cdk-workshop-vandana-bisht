b933da568fcedaf6ccceff98f2e719d1385576c4
Vandana Bisht <vandana.bisht@accolitedigital.com>
2024-07-25 19:42:53 +0530

    pipeline code added

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { Construct } from 'constructs';
+import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         // Creates a CodeCommit repository called 'WorkshopRepoVandanaBisht'
-        new codecommit.Repository(this, 'WorkshopRepoVandanaBisht', {
+        const repo = new codecommit.Repository(this, 'WorkshopRepoVandanaBisht', {
             repositoryName: "WorkshopRepoVandanaBisht"
         });
 
-        // Pipeline code goes here
-    }
+        // The basic pipeline declaration. This sets the initial structure
+        // of our pipeline
+       const pipeline = new CodePipeline(this, 'Pipeline', {
+        pipelineName: 'WorkshopPipelineVandana',
+        synth: new CodeBuildStep('SynthStep', {
+                input: CodePipelineSource.codeCommit(repo, 'main'),
+                installCommands: [
+                    'npm install -g aws-cdk'
+                ],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ]
+            }
+        )
+    });
+  }
 }
