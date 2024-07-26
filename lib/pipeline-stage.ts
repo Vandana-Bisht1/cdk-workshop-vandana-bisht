b5e73dda41ac0a7c8fe98c13ab45bf90bf070514
Vandana Bisht <vandana.bisht@accolitedigital.com>
2024-07-26 13:14:37 +0530

    added validation test

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,11 +1,16 @@
 import { CdkWorkshopVandanaBishtStack } from './cdk-workshop-vandana-bisht-stack';
-import { Stage, StageProps } from 'aws-cdk-lib';
+import { CfnOutput, Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class WorkshopPipelineStage extends Stage {
+    public readonly hcViewerUrl: CfnOutput;
+    public readonly hcEndpoint: CfnOutput;
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
-        new CdkWorkshopVandanaBishtStack(this, 'WebServiceVandanaBisht');
+        const service = new CdkWorkshopVandanaBishtStack(this, 'WebServiceVandanaBisht');
+
+        this.hcEndpoint = service.hcEndpoint;
+        this.hcViewerUrl = service.hcViewerUrl;
     }
 }
