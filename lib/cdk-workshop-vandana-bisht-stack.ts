406beefef92a0f254fdaeb0db8b4abe0c801f369
Vandana Bisht <vandana.bisht@accolitedigital.com>
2024-07-26 13:00:15 +0530

    get endpoints code added

--- lib/cdk-workshop-vandana-bisht-stack.ts
+++ lib/cdk-workshop-vandana-bisht-stack.ts
@@ -6,8 +6,10 @@ import { TableViewer } from 'cdk-dynamo-table-viewer';
 import { Construct } from 'constructs';
 
 export class CdkWorkshopVandanaBishtStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    public readonly hcViewerUrl: cdk.CfnOutput;
+    public readonly hcEndpoint: cdk.CfnOutput;
 
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const hello = new lambda.Function(this, 'HelloHandler', {
@@ -21,14 +23,22 @@ export class CdkWorkshopVandanaBishtStack extends cdk.Stack {
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, 'Endpoint', {
+    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
     });
 
-    new TableViewer(this, 'ViewHitCounter', {
+    const tv = new TableViewer(this, 'ViewHitCounter', {
       title: 'Hello Hits',
       table: helloWithCounter.table,
       sortBy: '-hits'
+    });
+
+    this.hcEndpoint = new cdk.CfnOutput(this, 'GatewayUrl', {
+      value: gateway.url
+    });
+
+    this.hcViewerUrl = new cdk.CfnOutput(this, 'TableViewerUrl', {
+      value: tv.endpoint
     });
   }
 }
